1fa1f8b0e47cc5bce7029605130b5ecfd2de7340
Andres <atoro1025@cue.edu.co>
2022-07-31 20:12:44 -0500

    Adelanto, solo falta funcionalidad prestamos

--- Taller 1/src/co/edu/cue/proyecto/Main.java
+++ Taller 1/src/co/edu/cue/proyecto/Main.java
@@ -1,7 +1,6 @@
 package co.edu.cue.proyecto;
 
 import javax.swing.*;
-import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
@@ -69,25 +68,11 @@ public class Main {
                             menuProductos=JOptionPane.showOptionDialog(
                                     null,"Ingrese lo que quiere hacer: ", "Opciones de productos", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,    // null para icono por defecto.
                                     new Object[] { "Consultar producto por codigo","Consultar prestamos por nombre", "Consultar cantidad total de unidades prestadas", "Remplazar objeto","Salir" },"null");
-                            switch (menuProductos){
-                                case 0:
-                                    double codigo;
-                                    consultarCodigo(listaProducto,codigo=Double.parseDouble(JOptionPane.showInputDialog("ingrese el codigo del producto")));
-                                    break;
-                                case 1:
-                                    String nombre;
-                                    consultarPrestamos(listaProducto,nombre=JOptionPane.showInputDialog("Digite el nombre del producto"));
-                                    break;
-                                case 2:
-                                    double code;
-                                    consultarUnidadesPrestadas(listaProducto,code=Double.parseDouble(JOptionPane.showInputDialog("ingrese el codigo del producto")));
-                                    break;
-                                case 3:
-                                    remplazarObjeto(listaProducto);
-                                    break;
-                                default:
-
-                                    break;
+                            switch (menuProductos) {
+                                case 0 -> consultarCodigo(listaProducto, Double.parseDouble(JOptionPane.showInputDialog("ingrese el codigo del producto")));
+                                case 1 -> consultarPrestamos(listaProducto, JOptionPane.showInputDialog("Digite el nombre del producto"));
+                                case 2 -> consultarUnidadesPrestadas(listaProducto, Double.parseDouble(JOptionPane.showInputDialog("ingrese el codigo del producto")));
+                                case 3 -> remplazarObjeto(listaProducto);
                             }
 
                         }while (menuProductos!=4);
@@ -118,7 +103,7 @@ public class Main {
     }
 
     //Funciones productos __________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
-    static void consultarCodigo(Producto listaProducto[],double codigo){
+    static void consultarCodigo(Producto[] listaProducto, double codigo){
         boolean confirm=false;
         for (int x=0;x<3;x++){
             if (listaProducto[x].getCodeg()==codigo){
@@ -127,12 +112,12 @@ public class Main {
                 break;
             }
         }
-        if (confirm==false){
+        if (!confirm){
             JOptionPane.showMessageDialog(null,"El codigo no coincide con ninguno de los productos");
         }
     }
 
-    static void consultarPrestamos(Producto listaProducto[],String nombre){
+    static void consultarPrestamos(Producto[] listaProducto, String nombre){
         boolean confirm=false;
         for (int x=0;x<3;x++){
             if (listaProducto[x].getNombre().equalsIgnoreCase(nombre)){
@@ -146,7 +131,7 @@ public class Main {
         }
     }
 
-    static void consultarUnidadesPrestadas(Producto listaProducto[],double codigo){
+    static void consultarUnidadesPrestadas(Producto[] listaProducto, double codigo){
         boolean confirm=false;
         for (int x=0;x<3;x++){
             if (listaProducto[x].getCodeg()==codigo){
@@ -155,12 +140,12 @@ public class Main {
                 break;
             }
         }
-        if (confirm==false){
+        if (!confirm){
             JOptionPane.showMessageDialog(null,"El codigo no coincide con ninguno de los productos");
         }
     }
 
-    static void remplazarObjeto(Producto listaProducto[]){
+    static void remplazarObjeto(Producto[] listaProducto){
         String message="";
         for(int x=0;x<3;x++){
             message+="El producto "+(x+1)+" es: "+listaProducto[x].getNombre()+"  Codigo: "+listaProducto[x].getCodeg()+ "  Unidades: "+listaProducto[x].getUnid()+"  Disponibilidad: "+listaProducto[x].getDisponibilidad()+"   Precio: "+listaProducto[x].getPrecio()+"  Veces de prestamo: "+ listaProducto[x].getVecesPrestamo()+"  Unidades prestadas: "+listaProducto[x].getCantidadPrestamo()+"\n"+"\n";
@@ -168,19 +153,13 @@ public class Main {
         int option=JOptionPane.showOptionDialog(
                 null,"Ingrese el producto que quiere remplazar "+"\n"+message, "Opciones de productos", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,    // null para icono por defecto.
                 new Object[] {  "Producto 1","Producto 2","Producto 3" },"null");
-        switch (option){
-            case 0:
-                listaProducto[option].setProducto(JOptionPane.showInputDialog("Digite el nombre del nuevo producto"),Double.parseDouble(JOptionPane.showInputDialog("Digite el codigo del nuevo producto")),Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de unidades del producto")),Boolean.parseBoolean(JOptionPane.showInputDialog("Digite la disponibilidad del producto (true or false)")),Double.parseDouble(JOptionPane.showInputDialog("Digite el precio del nuevo producto")),0,0);
-                break;
-            case 1:
-                listaProducto[option].setProducto(JOptionPane.showInputDialog("Digite el nombre del nuevo producto"),Double.parseDouble(JOptionPane.showInputDialog("Digite el codigo del nuevo producto")),Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de unidades del producto")),Boolean.parseBoolean(JOptionPane.showInputDialog("Digite la disponibilidad del producto (true or false)")),Double.parseDouble(JOptionPane.showInputDialog("Digite el precio del nuevo producto")),0,0);
-                break;
-            case 2:
-                listaProducto[option].setProducto(JOptionPane.showInputDialog("Digite el nombre del nuevo producto"),Double.parseDouble(JOptionPane.showInputDialog("Digite el codigo del nuevo producto")),Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de unidades del producto")),Boolean.parseBoolean(JOptionPane.showInputDialog("Digite la disponibilidad del producto (true or false)")),Double.parseDouble(JOptionPane.showInputDialog("Digite el precio del nuevo producto")),0,0);
-                break;
+        switch (option) {
+            case 0 -> listaProducto[option].setProducto(JOptionPane.showInputDialog("Digite el nombre del nuevo producto"), Double.parseDouble(JOptionPane.showInputDialog("Digite el codigo del nuevo producto")), Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de unidades del producto")), Boolean.parseBoolean(JOptionPane.showInputDialog("Digite la disponibilidad del producto (true or false)")), Double.parseDouble(JOptionPane.showInputDialog("Digite el precio del nuevo producto")), 0, 0);
+            case 1 -> listaProducto[option].setProducto(JOptionPane.showInputDialog("Digite el nombre del nuevo producto"), Double.parseDouble(JOptionPane.showInputDialog("Digite el codigo del nuevo producto")), Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de unidades del producto")), Boolean.parseBoolean(JOptionPane.showInputDialog("Digite la disponibilidad del producto (true or false)")), Double.parseDouble(JOptionPane.showInputDialog("Digite el precio del nuevo producto")), 0, 0);
+            case 2 -> listaProducto[option].setProducto(JOptionPane.showInputDialog("Digite el nombre del nuevo producto"), Double.parseDouble(JOptionPane.showInputDialog("Digite el codigo del nuevo producto")), Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de unidades del producto")), Boolean.parseBoolean(JOptionPane.showInputDialog("Digite la disponibilidad del producto (true or false)")), Double.parseDouble(JOptionPane.showInputDialog("Digite el precio del nuevo producto")), 0, 0);
         }
         JOptionPane.showMessageDialog(null,"El producto se guardo como: \n"+"Producto "+(option+1)+": "+listaProducto[option].getNombre()+"   Codigo: "+listaProducto[option].getCodeg()+"   Unidades: "+listaProducto[option].getUnid()+"  Disponibilidad: "+listaProducto[option].getDisponibilidad()+"   Precio: "+listaProducto[option].getPrecio()+"   Veces en prestamo: "+listaProducto[option].getVecesPrestamo()+"  Unidades en prestamo: "+listaProducto[option].getCantidadPrestamo());
     }
 
-
+//funciones de Prestamo_____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
 }
